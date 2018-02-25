68268ee09ad6f744ad4569c75a7c0ceecdfaee3f
Aiden Benner <aiden.benner@gmail.com>
2018-02-25 00:08:04 -0800

    Static components

--- static/src/App.js
+++ static/src/App.js
@@ -2,17 +2,60 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Timer extends Component {
+    render() {
+        return (
+            <div id="timer">
+                <TimeSetButton text="Pomodoro" />
+                <TimeSetButton text="Short Break"/>
+                <TimeSetButton text="Long Break"/>
+                <TimeSetButton text="Custom" />
+                <TimerText time="25:00"/> 
+                <TimeControlButton text="Start"/>
+                <TimeControlButton text="Stop"/>
+                <TimeControlButton text="Reset"/>
+            </div>
+        )
+    }
+}
+
+class TimeSetButton extends Component {
+    render() {
+        return (
+            <button>
+                {this.props.text}
+            </button>
+        )
+    }
+}
+
+
+
+class TimerText extends Component {
+    render() {
+        return (
+            <p> 
+                {this.props.time}
+            </p>
+        )
+    }
+}
+
+class TimeControlButton extends Component {
+    render() {
+        return (
+            <button>
+                {this.props.text}
+            </button>
+        )
+    }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Timer />
       </div>
     );
   }
